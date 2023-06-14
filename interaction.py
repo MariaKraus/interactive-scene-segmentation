e76ee29d2446b319def446e4039c28293765ad30
Daniel <boesch.daniel98@gmail.com>
2023-06-14 10:52:09 +0200

    work on mask selection

--- interaction.py
+++ interaction.py
@@ -3,15 +3,30 @@ import numpy as np
 
 selected_masks = []
 
+
+def is_dictionary_in_list(dictionary, dictionary_list):
+    for d in dictionary_list:
+        if np.array_equal(d['segmentation'], dictionary['segmentation']):
+            return True
+    return False
+
+
+def remove_mask(mask):
+    global selected_masks
+    for i, d in enumerate(selected_masks):
+        if np.array_equal(d['segmentation'], mask['segmentation']):
+            np.delete(selected_masks, i)
+            break
+
+
 def point_selection(event, selected_points, x, y, image, masks):
-    selected_mask = None
     selected_points.append((x, y))
     print(f"Selected point: ({x}, {y})")
 
     for mask in masks:
         if mask['segmentation'][y][x] == 1:
-            if mask in selected_masks:
-                selected_masks.remove(mask)
+            if is_dictionary_in_list(mask, selected_masks):
+                remove_mask(mask)
             else:
                 selected_mask = mask
                 selected_mask['color'] = list(np.random.choice(range(256), size=3))
@@ -19,9 +34,6 @@ def point_selection(event, selected_points, x, y, image, masks):
             break
 
     return selected_masks
-
-
-
 
 
 def draw_rect(event, x, y, flags, param):
