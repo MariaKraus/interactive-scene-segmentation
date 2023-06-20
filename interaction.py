ab9ce3529e74ad19edbc8e6abb260f88784e571b
Daniel <boesch.daniel98@gmail.com>
2023-06-20 22:29:28 +0200

    work on square mask selection

--- interaction.py
+++ interaction.py
@@ -34,27 +34,3 @@ def point_selection(event, selected_points, x, y, image, masks):
             break
 
     return selected_masks
-
-
-def draw_rect(event, x, y, flags, param):
-    global x1, y1, drawing, radius, num, img, img2, end_point
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        x1, y1 = x, y
-        cv2.rectangle(img, (x1, y1), (x1, y1), (255, 0, 0), 1)
-
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            a, b = x, y
-            if a != x & b != y:
-                img = img2.copy()
-                end_point = (a, b)
-                cv2.rectangle(img, (x1, y1), end_point, (255, 0, 0), 1)
-
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        end_point = (x, y)
-        cv2.rectangle(img, (x1, y1), end_point, (255, 0, 0), 1)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, '_'.join(['label', str(num)]), (x + 20, y + 20), font, 1, (200, 255, 155), 1, cv2.LINE_AA)
-        img2 = img.copy()
