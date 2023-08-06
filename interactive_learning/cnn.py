3ce32ac42c8e65d8a7f1f20cf0de8a60d2721ff3
MariaKraus <uorcz@student.kit.edu>
2023-08-06 15:23:59 +0200

    Adds training and labeling option for cnn.

--- interactive_learning/cnn.py
+++ interactive_learning/cnn.py
@@ -7,14 +7,18 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import torchvision.transforms as transforms
+from matplotlib import pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 from torchsummary import summary
 
 
 class CNN(nn.Module):
+    # Defining the CNN architecture
 
     def __init__(self):
         super(CNN, self).__init__()
+
+        # Convolutional layers for feature extraction
         self.conv_layers = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -27,6 +31,7 @@ class CNN(nn.Module):
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
+        # Fully connected layers for regression
         self.fc_layers = nn.Sequential(
             nn.Linear(in_features=12, out_features=48),
             nn.ReLU(),
@@ -38,11 +43,11 @@ class CNN(nn.Module):
         )
 
     def forward(self, x):
+        # Forward pass through layers
         x = self.conv_layers(x)
         x = self.fc_layers(x)
         x = x.view(1, -1)
         x = self.fc_layers2(x)
-        print(x)
         return x
 
 
@@ -66,8 +71,10 @@ class CNNTrainer:
     def __init__(self):
         self.epoch = 0
         self.model = CNN()
+        # optimizer
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
         self.criterion = nn.MSELoss()
+        self.losses = []
 
     def train_one_epoch(self, trainloader):
         running_loss = 0.0
@@ -81,12 +88,15 @@ class CNNTrainer:
             outputs = self.model(inputs)
             loss = self.criterion(outputs.squeeze(), labels.squeeze())
             loss.backward()
+            # clip gradients to prevent exploding gradients
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(),10)
             self.optimizer.step()
 
             running_loss += loss.item()
             last_loss = loss.item()
-
-            print('[%d, %5d] loss: %.3f' % (self.epoch + 1, i + 1, running_loss / 2000))
+            print("output ", outputs.squeeze())
+            print("label ", labels.squeeze())
+            print('[%d, %5d] loss: %.3f' % (self.epoch, i + 1, running_loss / 2000))
             running_loss = 0.0
 
         return last_loss
@@ -105,17 +115,30 @@ class CNNTrainer:
         trainloader = data.DataLoader(dataset, batch_size=1, num_workers=1)
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 
+        # Initialize Tensorboard
         writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
         loss = self.train_one_epoch(trainloader)
 
+        self.losses.append(loss)
         # TODO: potentially think of a way to add a validation set (if that even makes sense)
-
-        writer.add_scalar('Loss/train', loss, self.epoch)
-        writer.flush()
+        # log the loss
+        #writer.add_scalar('Loss/train', loss, self.epoch)
+        #writer.flush()
 
         path = os.getcwd() + '/models'
         if not os.path.exists(path):
             os.makedirs(path)
 
+        # save model
         model_path = 'models/interactive_model_{}.pth'.format(timestamp, self.epoch)
-        torch.save(self.model.state_dict(), model_path)
+        #torch.save(self.model.state_dict(), model_path)
+
+    def plot_results(self):
+        # Plot loss over epochs
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.losses, label='Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Training Loss')
+        plt.legend()
+        plt.show()
---
+++ train.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+
+import cv2
+
+from interactive_learning import cnn
+
+
+def load_images(directory: str):
+    # Read image from user input
+    directory_path = ""
+    images = []
+    # directory exists
+    while not images:
+        for filename in os.listdir(directory):
+            img = cv2.imread(os.path.join(directory, filename))
+            if img is not None:
+                images.append(img)
+    return images
+
+def read_numbers_from_file(filename):
+    numbers = []
+    with open(filename, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            numbers.append(number)
+    return numbers
+
+def train(image_dir: str, label:str):
+    """
+    Train the model
+    :param image_dir: The directory with the training images
+    :param label: The directory with the training labels
+    :return: None
+    """
+    images = load_images(image_dir)
+    numbers = read_numbers_from_file(label)
+
+    print("images", len(images))
+    print("numbers", len(numbers))
+    interactive_trainer = cnn.CNNTrainer()
+
+    for i in range(len(images)):
+        image_resized = cv2.resize(images[i], (100, 100))
+        #interactive_trainer.update(image_resized, int(numbers[i]))
+
+    hamburg_names = []
+    hamburg_numbers = []
+    # Read the text file line by line
+    with open(os.getcwd() + "/label_hamburg.txt", 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            parts = line.strip().split(',')
+            if len(parts) == 2:
+                hamburg_names.append(parts[0])
+                hamburg_numbers.append(int(parts[1]))
+
+    for i in range(len(hamburg_names)):
+        img = cv2.imread(os.path.join(os.getcwd(), "train", "hamburg", hamburg_names[i]))
+        image_resized = cv2.resize(img, (100, 100))
+        interactive_trainer.update(image_resized, hamburg_numbers[i])
+
+    interactive_trainer.plot_results()
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Interactive Scene Segmentation")
+    parser.add_argument("--i_dir", type=str, default=os.getcwd() + "/train/ulm/",
+                        help="The directory with the training images")
+    parser.add_argument("--label", type=str, default=os.getcwd() + "/label.txt",
+                        help="The directory with the training labels")
+
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.i_dir):
+        print("The given directory: ", args.dir, " does not exist.")
+        exit(0)
+
+    train(image_dir=args.i_dir, label=args.label)
