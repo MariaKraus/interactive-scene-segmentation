e5409084538f11bf167e12110dd693d35941e54a
MariaKraus <uorcz@student.kit.edu>
2023-08-24 12:44:28 +0200

    Updates training to work on all networks.

--- train.py
+++ train.py
@@ -1,10 +1,28 @@
 import argparse
 import os
-
 import cv2
+import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+from tqdm import tqdm
 from interactive_learning import cnn
+from interactive_learning import cnn_pretrained
+from interactive_learning import cnn_classification
 from sklearn.model_selection import train_test_split
+import torchvision.transforms as transforms
+
+
+def apply_custom_transform(image):
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        transforms.RandomGrayscale(),
+        transforms.GaussianBlur(3),
+    ])
+
+    transformed_image = transform(image)
+    return transformed_image
 
 
 def load_images(directory: str):
@@ -19,6 +37,7 @@ def load_images(directory: str):
                 images.append(img)
     return images
 
+
 def read_numbers_from_file(filename):
     numbers = []
     with open(filename, 'r') as file:
@@ -27,23 +46,24 @@ def read_numbers_from_file(filename):
             numbers.append(number)
     return numbers
 
-def train(image_dir: str, label:str):
+
+def train(image_dir: str, label: str, model: int, augmentations: int, epochs : int):
     """
     Train the model
+    :param epochs:
+    :param augmentations: The number of augmentations to apply to each image
+    :param model: The model to use
     :param image_dir: The directory with the training images
     :param label: The directory with the training labels
     :return: None
     """
-    #images = load_images(image_dir)
-    #numbers = read_numbers_from_file(label)
-
-    #print("images", len(images))
-    #print("numbers", len(numbers))
-    interactive_trainer = cnn.CNNTrainer()
-
-    #for i in range(len(images)):
-        # image_resized = cv2.resize(images[i], (200, 200))
-        #interactive_trainer.update(image_resized, int(numbers[i]))
+    interactive_trainer = None
+    if model == 1:
+        interactive_trainer = cnn.CNNTrainer()
+    elif model == 2:
+        interactive_trainer = cnn_pretrained.CNNTrainer()
+    elif model == 3:
+        interactive_trainer = cnn_classification.CNNTrainer()
 
     names = []
     numbers = []
@@ -58,7 +78,7 @@ def train(image_dir: str, label:str):
 
     # Read the text file line by line
     with open(os.getcwd() + "/label_cats.txt", 'r') as file:
-        lines  = file.readlines()
+        lines = file.readlines()
         for line in lines:
             parts = line.strip().split(',')
             if len(parts) == 2:
@@ -66,25 +86,37 @@ def train(image_dir: str, label:str):
                 numbers.append(int(parts[1]))
 
     # Split the data into training and validation sets
-    train_data, val_data, train_labels, val_labels = train_test_split(names, numbers, test_size=0.2, random_state=42)
+    data, labels = [], []
 
-    epochs = 10
+    # Load the images and augment them
+    for i in tqdm(range(len(names)), desc="Loading and Augmenting Images", unit="image"):
+        img = cv2.imread(os.path.join(os.getcwd(), "train", "cats", names[i]))
+        # resize otherwise it takes too long
+        img = cv2.resize(img, (224, 224))
+        data.append(img)
+        labels.append(numbers[i])
+        for j in range(augmentations):
+            img_augmented = apply_custom_transform(img)
+            data.append(img_augmented)
+            labels.append(numbers[i])
+
+    # Split the data into training and validation sets
+    train_data, val_data, train_labels, val_labels = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+    # Train the model for a fixed numer of epochs
+    epochs = epochs
     for ep in range(epochs):
-        print("Epoch: ", ep)
+        print("Epoch: \n", ep)
         train_data, train_labels = shuffle(train_data, train_labels)
-        for i in range(len(train_data)):
-            img = cv2.imread(os.path.join(os.getcwd(), "train", "cats", train_data[i]))
-            image_resized = cv2.resize(img, (100, 100))
-            interactive_trainer.update(image_resized, train_labels[i])
+        for i in tqdm(range(len(train_data)), desc="training", unit="image"):
+            interactive_trainer.update(train_data[i], train_labels[i])
 
         # validate at the end of each epoch
-        for k in range(len(val_data)):
-            img = cv2.imread(os.path.join(os.getcwd(), "train", "cats", val_data[k]))
-            image_resized = cv2.resize(img, (100, 100))
-            interactive_trainer.validate(image_resized, val_labels[k])
+        for k in tqdm(range(len(val_data)), desc="validation", unit="image"):
+            interactive_trainer.validate(val_data[k], val_labels[k])
 
     interactive_trainer.plot_results()
-    # interactive_trainer.save_model()
+    interactive_trainer.save_model()
 
 
 if __name__ == "__main__":
@@ -93,6 +125,12 @@ if __name__ == "__main__":
                         help="The directory with the training images")
     parser.add_argument("--label", type=str, default=os.getcwd() + "/label.txt",
                         help="The directory with the training labels")
+    parser.add_argument("--model", type=int, default=3,  # 1 = cnn, 2 = vgg16, 3 = classifier
+                        help="The model to use for training")
+    parser.add_argument("--augmentations", type=int, default=5,
+                        help="The number of augmentations to use for training")
+    parser.add_argument("--epochs", type=int, default=5,
+                        help="The number of augmentations to use for training")
 
     args = parser.parse_args()
 
@@ -100,4 +138,4 @@ if __name__ == "__main__":
         print("The given directory: ", args.dir, " does not exist.")
         exit(0)
 
-    train(image_dir=args.i_dir, label=args.label)
+    train(image_dir=args.i_dir, label=args.label, model=args.model, augmentations=args.augmentations, epochs=args.epochs)
